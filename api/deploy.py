67d0ff4a0ad8f22cc66904dd7a86d2160fc50764
saurabh <saurabhgupta3012@gmail.com>
2018-11-28 19:55:57 +0530

    api textrank with form done

--- api/deploy.py
+++ api/deploy.py
@@ -1,41 +1,51 @@
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from summarization import text_rank
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/YOURAPP/*": {"origins": "*"}})
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
 
         if request.method == 'POST':
-            rq = request.get_json()
-            text = rq['text']
+            # print("req data", request.form)
+            # rq = request.get_json()['text']
+            text = request.form['para']
+            summ = text_rank(text)
             #text = text.decode('utf-8')
             #print(text)
 
-            return text_rank(text)
-        #         if 'file' not in request.files:
-        #                 print('No file part')
-        #                 return redirect(request.url)
-
-        #         file = request.files['file']
-        #         if file.filename == '':
-        #                 print('No selected file')
-        #                 return redirect(request.url)
-        #         if file:
-        #                 filename = secure_filename(file.filename)
-        #                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #                 filepath = (app.config['UPLOAD_FOLDER'] + '/' +filename)
-
-        #                 base = filepath
-        #                 basename = os.path.splitext(base)[0]
-        #                 if not os.path.exists(basename):
-        #                     os.makedirs(basename)
-        #                     video_frame(filepath, basename)
-                        
-
-        #                 rstf = main_func(basename)
-        #                 return (str(rstf))
+            return '''
+                    <h3> Input your text here:</h3>
+                    <form method="POST">
+                              <textarea name="para" cols="150" rows="15">'''+text+'''</textarea><br>
+                              <select name="summ_met">
+                                  <option value="tx" selected>Text Rank</option>
+                                  <option value="fb">Feature based</option>
+                              </select>
+                              <h6> Input Length: '''+str(len(text))+'''</h6>
+                              <input type="submit" value="Submit">
+                    </form>
+                    <br><br>
+                    <h3> Summary of the text: </h3>
+                    <form >
+                              <textarea name="parares" cols="150" rows="15">'''+summ+'''</textarea>
+                              <h6> Summary Length: '''+str(len(summ))+'''</h6>
+                              <br>
+                    </form>'''
 
         else:
-             	return "Make a post request" 
+             	return '''
+                        <h3> Input your text here:</h3>
+                        <form method="POST">
+                              <textarea name="para" cols="150" rows="15">Enter your code-mix article here</textarea><br>
+                              <select name="summ_met">
+                                  <option value="tx" selected>Text Rank</option>
+                                  <option value="fb">Feature based</option>
+                              </select>
+                              <input type="submit" value="Submit"><br>
+                          </form>''' 
